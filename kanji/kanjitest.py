0bd613f4d7460da825ca1f01e13ebdbdbae19782
Takumi Sueda <puhitaku@gmail.com>
2019-12-28 21:57:50 +0900

    Improve test

--- kanji/kanjitest.py
+++ kanji/kanjitest.py
@@ -1,17 +1,17 @@
 import unittest
-from kanji_conversion1 import kanji_conversion, kanji
+from kanji_conversion import convert
 
-chars = ['一', '二', '三', '四', '五', '六', '七', '八', '九', '十', '百', '千']
-kanji_suu = input('漢数字を入力してください')
-kanli = list(kanji_suu)
+
 class Testkanji(unittest.TestCase):
-    def test_conversion(self):
-        inter = set(chars) & set(kanli)
-        self.assertEqual(inter, set(kanli))
+    def test_convert_simple(self):
+        self.assertEqual(convert('三百九十七'), 397)
 
-    def test_notconversion(self):
-        exclu = set(chars) ^ set(kanli)
-        self.assertNotEqual(exclu, kanli)
+    def test_convert_oku(self):
+        self.assertEqual(convert('一億一'), 1)
+
+    def test_convert_oku(self):
+        self.assertEqual(convert('一億二千四百三十七万五千八百五十四'), 124375854)
 
 if __name__ == '__main__':
     unittest.main()
+
