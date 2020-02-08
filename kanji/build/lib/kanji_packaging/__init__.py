7aa79ba9fecc0303d249d3773eb06d1e8aacfee8
bunbunjin <bunbunjin.aisu@gmail.com>
2020-02-08 20:21:37 +0900

    bulidが何かわからない

---
+++ kanji/build/lib/kanji_packaging/__init__.py
@@ -0,0 +1,2 @@
+from .kanji_conversion import convert
+__all__ = ['kanji_conversion', 'kanji_main']
---
+++ kanji/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(
+    name='kanji',
+    version='0.0.1',
+    description='Convert kanji',
+    packages=['kanji_packaging'],
+)
