b41ed41a8f7169e2eee2a44ecfcc43f7b67427ba
bunbunjin <bunbunjin.aisu@gmail.com>
2020-05-18 09:28:52 +0900

    kanji_main.pyからmain.pyに変更した

---
+++ kanji/kanji/main.py
@@ -0,0 +1,10 @@
+from kanji_conversion import convert
+
+kanji_suu = input('漢数字を入力してください')
+chars = ['一', '二', '三', '四', '五', '六', '七', '八', '九', '十', '百', '千', '万', '億']
+
+if any(c not in chars for c in kanji_suu):
+    raise ValueError('Unknown kanji was input')
+
+else:
+    convert(kanji_suu)
